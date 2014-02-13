da21790ad128fa9c865dd8a5c680637fcdbdb87b
gogotanaka <yakuruto771@live.jp>
2014-02-13 18:06:58 +0900

    delete DS_Store

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,11 @@
 class WelcomeController < ApplicationController
   def index
     @user_id = cookies.permanent[:user_id]
-  	@amount_hash = {"特盛" => 4, "大盛り" => 3, "普通" => 2, "少なめ" => 1, "ご飯1/3" => 0}
+    @amount_hash = {"特盛" => 4, "大盛り" => 3, "普通" => 2, "少なめ" => 1, "ご飯1/3" => 0}
   end
 
   def multiply_menu_ajax
-  	@amount_hash = {"特盛" => 4, "大盛り" => 3, "普通" => 2, "少なめ" => 1, "ご飯1/3" => 0}
-  	@index = params[:index].to_i + 1
+    @amount_hash = {"特盛" => 4, "大盛り" => 3, "普通" => 2, "少なめ" => 1, "ご飯1/3" => 0}
+    @index = params[:index].to_i + 1
   end
 end
